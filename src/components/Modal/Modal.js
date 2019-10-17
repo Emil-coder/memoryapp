3e9fb8be026cdd9a9a71f2316288a4a86374d3c7
Emil Voltaire <emil.voltaire@54u.se>
2019-10-17 15:27:54 +0200

    handleSubmit prints out nextId, player and score.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,17 +4,28 @@ const Modal = props => {
   if (!props.show) {
     return null;
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('inside handleSubmit');
+    console.log(event.target.elements.nextId.value);
+    console.log(event.target.elements.player.value);
+    console.log(event.target.elements.score.value);
+  };
+
+
   console.log('inside Modal');
   console.log('nextId:' + props.nextId);
   return (
     <>
       <div className="modal-container">
         <h1>Congratulations!<span>You won the game!</span></h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="section"><span>1</span>Player & Score</div>
           <div className="inner-wrap">
             <input type="hidden" name="nextId" value={props.nextId} />
-            <label>Player <input type="text" name="player" id="player" /></label>
+            <input type="hidden" name="score" value={props.score} />
+            <label>Player <input type="text" name="player" placeholder="Your name here..." /></label>
             <label>Score: {props.score}s</label>
           </div>
 
